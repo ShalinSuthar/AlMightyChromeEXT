c216aab67a8875d404d6f4e4309fc3ee383be268
tomasesber <tomas.esber@duke.edu>
2025-02-04 20:46:48 -0600

    store indexes bounded by modulo

--- js/quote.js
+++ js/quote.js
@@ -16,15 +16,29 @@ document.addEventListener('DOMContentLoaded', () => {
     // fetch user theme preference
     chrome.storage.sync.get('preferredTheme', (data) => {
       let selectedTheme = data.preferredTheme || 'default';
+      let indexKey = `${selectedTheme}Index`;
 
-      // fetch quotes from our backend API
-      fetch(`https://ntbvju14ce.execute-api.us-east-1.amazonaws.com/dev/quote?category=${selectedTheme}`)
-        .then(response => response.json())
-        .then(data => {
-            const randomQuote = data.text;
+      chrome.storage.sync.get(indexKey, (indexData) => {
+        let index = indexData[indexKey] || 0;
+
+        // fetch quotes from our backend API with two query params: index and category
+        fetch(`https://ntbvju14ce.execute-api.us-east-1.amazonaws.com/dev/quote?category=${selectedTheme}&index=${index}`)
+          .then(response => response.json())
+          .then(data => {
+            const randomQuote = data.text.text;
+            // render quote
             quoteContainer.textContent = randomQuote;
-        })
-        .catch(error => console.error('Error fetching quote:', error));
+            
+            // modulo makes sure we don't hit an out of bounds error, and loops us back to the first quote
+            // once customer has seen all the quotes for this category
+            let nextIndex = (index + 1) % data.totalQuotes;
+            console.log()
+            // update index in browser storage
+            // for storage limits, see: https://developer.chrome.com/docs/extensions/reference/api/storage
+            chrome.storage.sync.set({ [indexKey]: nextIndex });
+          })
+          .catch(error => console.error('Error fetching quote:', error));
+      });
     });
   }
 });
