2bd63bd12bc553e7609c16ba30e92ea2afdee6b2
ShalinSuthar <shalinsuthar2000@gmail.com>
2025-03-02 21:09:29 -0600

    trivia difficulty based bg fixed

--- js/trivia.js
+++ js/trivia.js
@@ -1,9 +1,8 @@
-// trivia widget renders trivia questions
 const triviaWidget = {
     id: "trivia",
     name: "Trivia",
     render: function() {
-      this.loadAndDisplayTriviaQuestion();
+        this.loadAndDisplayTriviaQuestion();
     },
     hide: function() {
         const widgetElement = document.getElementById('trivia-container');
@@ -12,36 +11,41 @@ const triviaWidget = {
         }
     },
     loadAndDisplayTriviaQuestion: function() {
-      // fetch user theme preference from the browser
-      chrome.storage.sync.get(['triviaScore', 'triviaX', 'triviaY'], (browserData) => {
-        let triviaScore = browserData.triviaScore || 0;
-        // fetch trivia from our backend API with one query param: user's current trivia score
-        fetch(`https://ntbvju14ce.execute-api.us-east-1.amazonaws.com/dev/trivia?score=${triviaScore}`)
-        .then(response => response.json())
-        .then(apiData => {
-            this.displayTriviaQuestion(apiData, browserData, triviaScore);
+        // Fetch user theme preference from the browser
+        chrome.storage.sync.get(['triviaScore', 'triviaX', 'triviaY'], (browserData) => {
+            let triviaScore = browserData.triviaScore || 0;
 
-        })
-        .catch(error => console.error('Error fetching trivia:', error));
-      });
+            // Fetch trivia from our backend API with one query param: user's current trivia score
+            fetch(`https://ntbvju14ce.execute-api.us-east-1.amazonaws.com/dev/trivia?score=${triviaScore}`)
+            .then(response => response.json())
+            .then(apiData => {
+                this.displayTriviaQuestion(apiData, browserData, triviaScore);
+            })
+            .catch(error => console.error('Error fetching trivia:', error));
+        });
     },
     displayTriviaQuestion: function(apiData, browserData, triviaScore) {
         const widgetElement = document.getElementById('trivia-container');
         const questionElement = document.getElementById('trivia-question');
         const optionsContainer = document.getElementById('trivia-options-container');
         const scoreElement = document.getElementById('trivia-score');
-        
+        const difficultyElement = document.getElementById('trivia-difficulty');
+
         let triviaX = browserData.triviaX;
         let triviaY = browserData.triviaY;
         widgetElement.style.left = `${triviaX}px`;
         widgetElement.style.top = `${triviaY}px`;
-    
+
         questionElement.textContent = apiData.question;
         optionsContainer.innerHTML = "";
-    
+
+        // Set Score and Difficulty
         scoreElement.textContent = `Score: ${triviaScore}`;
-    
-        widgetElement.style.display = "block"; 
+        difficultyElement.textContent = `Current Difficulty: ${apiData.difficulty}`;
+        widgetElement.className = `${apiData.difficulty.toLowerCase()}`;
+
+        widgetElement.style.display = "block";
+
         Object.entries(apiData.options).forEach(([key, value]) => {
             const optionButton = document.createElement('button');
             optionButton.textContent = `${key}. ${value}`;
@@ -65,7 +69,7 @@ const triviaWidget = {
                 this.loadAndDisplayTriviaQuestion();
             });
         }, 250);
- }    
-}    
-  
+    }
+};
+
 window.triviaWidget = triviaWidget;
